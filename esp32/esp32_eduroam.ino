3e3fc28d5cbd07be561b4b8cb1698b7f2fb8c177
Eric <31218485+16BitNarwhal@users.noreply.github.com>
2022-10-16 09:12:07 -0400

    test

---
+++ esp32/esp32_eduroam.ino
@@ -0,0 +1,132 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include <WiFiClient.h>
+#include "OV2640.h"
+
+OV2640 cam;
+
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+const char* ssid     = "Assistant Reader";
+const char* passphrase = "assistant_reader";
+
+WebServer server(80);
+
+const char HEADER[] = "HTTP/1.1 200 OK\r\n" \
+                      "Access-Control-Allow-Origin: *\r\n" \
+                      "Content-Type: multipart/x-mixed-replace; boundary=123456789000000000000987654321\r\n";
+const char BOUNDARY[] = "\r\n--123456789000000000000987654321\r\n";
+const char CTNTTYPE[] = "Content-Type: image/jpeg\r\nContent-Length: ";
+const int hdrLen = strlen(HEADER);
+const int bdrLen = strlen(BOUNDARY);
+const int cntLen = strlen(CTNTTYPE);
+
+void handle_jpg_stream(void)
+{
+  char buf[32];
+  int s;
+
+  WiFiClient client = server.client();
+
+  client.write(HEADER, hdrLen);
+  client.write(BOUNDARY, bdrLen);
+
+  while (true)
+  {
+    if (!client.connected()) break;
+    cam.run();
+    s = cam.getSize();
+    client.write(CTNTTYPE, cntLen);
+    sprintf( buf, "%d\r\n\r\n", s );
+    client.write(buf, strlen(buf));
+    client.write((char *)cam.getfb(), s);
+    client.write(BOUNDARY, bdrLen);
+  }
+}
+
+void handleNotFound()
+{
+  String message = "Server is running!\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  server.send(200, "text / plain", message);
+}
+
+void setup() {
+    
+    Serial.begin(115200);
+
+    camera_config_t config;
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = Y2_GPIO_NUM;
+    config.pin_d1 = Y3_GPIO_NUM;
+    config.pin_d2 = Y4_GPIO_NUM;
+    config.pin_d3 = Y5_GPIO_NUM;
+    config.pin_d4 = Y6_GPIO_NUM;
+    config.pin_d5 = Y7_GPIO_NUM;
+    config.pin_d6 = Y8_GPIO_NUM;
+    config.pin_d7 = Y9_GPIO_NUM;
+    config.pin_xclk = XCLK_GPIO_NUM;
+    config.pin_pclk = PCLK_GPIO_NUM;
+    config.pin_vsync = VSYNC_GPIO_NUM;
+    config.pin_href = HREF_GPIO_NUM;
+    config.pin_sscb_sda = SIOD_GPIO_NUM;
+    config.pin_sscb_scl = SIOC_GPIO_NUM;
+    config.pin_pwdn = PWDN_GPIO_NUM;
+    config.pin_reset = RESET_GPIO_NUM;
+    config.xclk_freq_hz = 20000000;
+    config.pixel_format = PIXFORMAT_JPEG;
+
+    config.frame_size = FRAMESIZE_QVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 2;
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Take Online ... ");
+    Serial.println(ssid);
+
+  /*
+   * Set up an access point
+   * @param ssid          Pointer to the SSID (max 63 char).
+   * @param passphrase    (for WPA2 min 8 char, for open use NULL)
+   * @param channel       WiFi channel number, 1 - 13.
+   * @param ssid_hidden   Network cloaking (0 = broadcast SSID, 1 = hide SSID)
+   */
+
+    cam.init(config);
+
+    WiFi.softAP(ssid, passphrase, 2, 0);
+
+    Serial.print("IP address: ");
+    Serial.println(WiFi.softAPIP());
+    
+    server.on("/mjpeg/1", HTTP_GET, handle_jpg_stream);
+    server.onNotFound(handleNotFound); 
+    server.begin();
+}
+ 
+void loop() {
+  server.handleClient();
+}
--- web/src/components/Cam.jsx
+++ web/src/components/Cam.jsx
@@ -1,20 +1,20 @@
-import React, {useState} from 'react';
-import '../Cam.css';
-import Webcam from "react-webcam";
+import React, { useRef, useState} from 'react';
+// import '../Cam.css';
+// import Webcam from "react-webcam";
 import Tesseract from "tesseract.js";
 import Container from 'react-bootstrap/Container';
 // import { useSpeechSynthesis } from "react-speech-kit";
 
-const videoConstraints = {
-    width: 440,
-    height: 400,
-    facingMode: "user"
-};
+// const videoConstraints = {
+//     width: 440,
+//     height: 400,
+//     facingMode: "user"
+// };
 
 export default function Cam() {
-    const webcamRef = React.useRef(null);
-    const [text, setText] = useState("");
-
+    // const webcamRef = React.useRef(null);
+    // const [text, setText] = useState("");
+    const image_ref = useRef(null);
     const msg = new SpeechSynthesisUtterance();
 
     async function speech(txt) {
@@ -41,46 +41,30 @@ export default function Cam() {
     //         .catch(err => console.error(err));
     // }
 
-    const handleClick = React.useCallback(
-        () => {
-            const imageSrc = webcamRef.current.getScreenshot();
-            Tesseract.recognize(
-                imageSrc, 'eng',
-                {
-                    logger: m => console.log(m > 50 ? m : '')
-                }
-            )
-                .catch (err => {
-                    console.error(err);
-                })
-                .then(result => {
-                    let text = "";
-                    if (result.data.confidence > 50) {
-                        for (let i of result.data.text)
-                            if ((i <= 'Z' && i >= 'A') || (i <= 'z' && i >= 'a') || (i >= '0' && i <= '9') || i === ' ' || i === ',' || i === '.' || i === '!' || i === '\'' || i === '"')
-                                text += i;
-                        console.log(text);
-                    }
+    async function text_rec() {
 
-                    setText(text);
-                    speech(text);
-                })
-        }, [webcamRef]);
+        const {data: {text}} = await Tesseract.recognize(
+            image_ref,
+            'eng',
+            { logger: m => console.log(m) }
+        );
+        console.log(text);
+        speech(text);
+    }
 
     return (
         <Container style={{marginTop: "40px", marginBottom: "40px", justifyContent: "center", alignItems: "center", display: "flex"}}>
             <div className="App">
                 <main className="App-main">
                     <h1>Extracted text</h1>
-                    <Webcam
-                        audio={false}
-                        ref={webcamRef}
-                        screenshotFormat="image/jpeg"
-                        videoConstraints={videoConstraints} />
-                    <div className="text-box" style={{display: "flex", justifyContent: "center"}}>
-                        <p style={{textAlign: "center", maxWidth: "1000px"}}> {text} </p>
-                    </div>
-                    <button onClick={handleClick} style={{height:50}}> convert to text</button>
+                    <img
+                        width="600"
+                        height="400"
+                        alt="stream"
+                        src="http://192.168.4.1/mjpeg/1"
+                        ref={image_ref}
+                    />
+                    <button onClick={text_rec} style={{height:50}}> convert to text</button>
                 </main>
             </div>
         </Container>
